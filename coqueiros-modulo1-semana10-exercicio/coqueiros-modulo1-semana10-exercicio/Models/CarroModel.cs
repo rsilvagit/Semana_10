dcde815a024a6cc6b989239cf8881f76a60c466f
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-04-05 20:36:09 -0300

    [M1S10] Ex 3 - Criar Model chamado CarroModel.cs

---
+++ coqueiros-modulo1-semana10-exercicio/coqueiros-modulo1-semana10-exercicio/Models/CarroModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace coqueiros_modulo1_semana10_exercicio.Models
+{
+    [Table("Carro")]
+    public class CarroModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        
+        [ForeignKey("MarcaModel")]
+        public List<MarcaModel>? Marca { get; set; }
+        public DateTime DataLocacao { get; set; }
+
+
+        
+        
+    }
+}
