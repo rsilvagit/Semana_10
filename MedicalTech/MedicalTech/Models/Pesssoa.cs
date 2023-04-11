1f473c07c44dff48fb7dc2512cab0c1d2766d448
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-04-10 21:49:07 -0300

    criação classe Pessoa

---
+++ MedicalTech/MedicalTech/Models/Pesssoa.cs
@@ -0,0 +1,11 @@
+﻿namespace MedicalTech.Models
+{
+    public class Pesssoa
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public string? Cpf { get; set; }
+        public string Telefone { get; set;}
+    }
+}
