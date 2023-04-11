b2a291a0933a811185cacbb29851869503e00b8c
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-04-10 22:02:59 -0300

    classe paciente

---
+++ MedicalTech/MedicalTech/Models/Paciente.cs
@@ -0,0 +1,11 @@
+﻿namespace MedicalTech.Models
+{
+    public class Paciente:Pesssoa
+    {
+        public string? ContatoDeEmergencia { get; set; }
+        public List<string>ListaDeAlergias { get; set; }
+        public List<string> ListaCuidadosEspecifios { get; set; }
+        public string Convenio { get; set; }
+        public string? StatusdeAtendimento { get; set; }
+    }
+}
