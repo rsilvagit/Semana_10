89c0223c50d0d52e9c8b3a7914a66462cb60900e
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-04-10 21:52:07 -0300

    atualização de pessoa

--- MedicalTech/MedicalTech/Models/Pesssoa.cs
+++ MedicalTech/MedicalTech/Models/Pesssoa.cs
@@ -3,7 +3,7 @@
     public class Pesssoa
     {
         public int Id { get; set; }
-        public string? Nome { get; set; }
+        public string? NomeCompleto { get; set; }
         public DateTime? DataNascimento { get; set; }
         public string? Cpf { get; set; }
         public string Telefone { get; set;}
