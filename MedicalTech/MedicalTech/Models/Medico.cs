14d6cc7bd8e87978e6a0b35e3927b2b27b48d347
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-04-11 23:54:06 -0300

    Medico

--- MedicalTech/MedicalTech/Models/Medico.cs
+++ MedicalTech/MedicalTech/Models/Medico.cs
@@ -2,6 +2,11 @@
 {
     public class Medico:Pesssoa
     {
-       
+       public string InstEnsinoForm { get; set; }
+       public string? Crm { get; set; }
+       public string? EspClinica { get; set; }
+       public bool StatusSistema { get; set; }
+       public int TotalAtendimentos { get; set; }
+
     }
 }
